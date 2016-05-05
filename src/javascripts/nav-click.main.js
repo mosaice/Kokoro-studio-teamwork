8467aaef9e8aae16c984414d042827c3ff16bb96
momo <354512218@qq.com>
2016-05-05 15:01:53 +0800

    小伟页面的整理

--- src/javascripts/nav-click.main.js
+++ src/javascripts/nav-click.main.js
@@ -3,33 +3,41 @@ $(function() {
   var $navChild = $('.nav-list-optional');
   var $ListSwitch = $('#list-switch');
   var $SwitchNav = $('#switch-list');
+  var $ClickRegion = $('#click-region');
+  var $Region = $('.mask');
 
-  NavClick($nav, $navChild);
+  NavClick($nav, $navChild, $ClickRegion);
   ClickSwitch($ListSwitch, $SwitchNav);
 
   $(".button-collapse").sideNav();
 })
 
-function NavClick(parent, child) {
+function NavClick(parent, child, Region) {
   parent.find(child).click(function() {
     var $Self = $(this),
       $Child = $(this).find('i');
-    if ($Self.hasClass('active') && $Child.hasClass('triangle_top')) {
-      $Child.removeClass('triangle_top');
-    } else if ($(this).hasClass('active')) {
-      $Child.addClass('triangle_top');
-    } else {
-      parent.find(child).removeClass('active');
-      $Self.addClass('active');
+    parent.find(child).removeClass('active');
+    $Self.addClass('active');
+    if(Region.parent(parent).hasClass('active')){
+      $('body').addClass('overflowY');
+      Region.siblings('.mask').css({
+        display:'block'
+      });
+    }else{
+      $('body').removeClass('overflowY');
+      parent.find('.mask').css({
+        display:'none'
+      })
     }
   });
+
 }
 
 function ClickSwitch(element, childelement) {
   element.click(function(event) {
-    if(childelement.hasClass('wedding-suti-list-cross')){
+    if (childelement.hasClass('wedding-suti-list-cross')) {
       childelement.removeClass('wedding-suti-list-cross').addClass('wedding-suti-list-upright');
-    }else{
+    } else {
       childelement.removeClass('wedding-suti-list-upright').addClass('wedding-suti-list-cross');
     }
   });
