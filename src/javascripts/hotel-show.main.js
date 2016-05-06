94e6027c825f7d39c249d8dafe12232ba11323f4
liuqisakuya <liuqi2208@gmail.com>
2016-05-06 22:41:52 +0800

    缺datepicker

---
+++ src/javascripts/hotel-show.main.js
@@ -0,0 +1,38 @@
+/**
+ * 多个页面通用的js放到common文件夹下
+ * 每个页面单独需要的js整理一个.main.js文件
+ * 自己写的代码带上作者 创建日期 修改日期 代码功能注释
+ * 修改他人代码需要额外注释
+ * 例子如下
+ */
+
+/**
+ * 作者: zhangweinan
+ * 作用: 婚宴菜单列表弹出窗口控制
+ * 创建日期: 2016-05-04
+ * 修改日期: 2016-05-04
+ */
+
+
+$(function () {
+	
+  $('.list-meal1 a').click(function () {
+	  console.log('111');
+    $('.wedding-menu-mask').fadeIn(300);
+	$('body').addClass('overflowY');
+  })
+  $('.wedding-menu-mask').click(function(){
+	  $('.wedding-menu-mask').fadeOut(300);
+	  $('body').removeClass('overflowY');
+  })
+  
+ }) 
+/**
+ * 作者: zhangweinan
+ * 作用: 婚宴酒店信息展示
+ * 创建日期: 2016-05-04
+ * 修改日期: 2016-05-04
+ */
+
+
+  
