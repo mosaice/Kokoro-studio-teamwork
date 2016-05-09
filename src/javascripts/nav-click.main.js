369c7c0a6c06680dc801dcff3784a261d39e946a
Zwn09266951 <178723651@qq.com>
2016-05-09 11:07:59 +0800

    修改3

--- src/javascripts/nav-click.main.js
+++ src/javascripts/nav-click.main.js
@@ -4,13 +4,7 @@ $(function() {
   var $SwitchNav = $('#switch-list');
   var $Region = $('#click-region');
   var $MaskClick = $('#mask-list');
-<<<<<<< HEAD
-  
 
-=======
-
-
->>>>>>> 9a6ea4baecfe776ba75f4c07811816d8a5ccc16e
   NavClick($nav, $Region, $MaskClick);
   ClickSwitch($ListSwitch, $SwitchNav);
   $(".button-collapse").sideNav();
@@ -18,15 +12,11 @@ $(function() {
 
 function NavClick(parent, Region, Mask) {
 
-<<<<<<< HEAD
-  parent.find('.nav-list-optional').click(function () {
-    var $Self = $(this)
-      , $Child = $(this).find('i');
-=======
+
   parent.find('.nav-list-optional').click(function() {
     var $Self = $(this),
       $Child = $(this).find('i');
->>>>>>> 9a6ea4baecfe776ba75f4c07811816d8a5ccc16e
+
     parent.find('.nav-list-optional').removeClass('active');
     $Self.addClass('active');
     if (parent.find('.nav-list-optional').eq(0).hasClass('active') && Mask.length != 0) {
@@ -41,20 +31,12 @@ function NavClick(parent, Region, Mask) {
       $('body').removeClass('overflowY');
     }
   });
-<<<<<<< HEAD
-  Mask.find('p').click(function (event) {
-    Mask.find('p').removeClass('active');
-    $(this).addClass('active');
-  });
-  Mask.click(function (event) {
-=======
-
   Mask.find('p').click(function(event) {
     Mask.find('p').removeClass('active');
     $(this).addClass('active');
   });
   Mask.click(function(event) {
->>>>>>> 9a6ea4baecfe776ba75f4c07811816d8a5ccc16e
+
     $('body').removeClass('overflowY');
     $(this).css('display', 'none');
   });
@@ -70,13 +52,3 @@ function ClickSwitch(element, childelement) {
     }
   });
 }
-<<<<<<< HEAD
-
-
-
-
-
-
-
-=======
->>>>>>> 9a6ea4baecfe776ba75f4c07811816d8a5ccc16e
---
+++ src/javascripts/wedding-dress-details.main.js
@@ -0,0 +1,32 @@
+/**
+ * 多个页面通用的js放到common文件夹下
+ * 每个页面单独需要的js整理一个.main.js文件
+ * 自己写的代码带上作者 创建日期 修改日期 代码功能注释
+ * 修改他人代码需要额外注释
+ * 例子如下
+ */
+
+/**
+ * 作者: zhangweinan
+ * 作用: 婚纱菜单列表弹出窗口控制
+ * 创建日期: 2016-05-04
+ * 修改日期: 2016-05-04
+ */
+
+
+$(function () {
+     //显示购物车列表
+	$('#addshop').click(function () {
+		$('.mask').fadeIn(300);
+		$('.details-add').fadeIn(300);
+		$('body').addClass('overflowY');
+	})
+	//点击遮罩退出
+	$('.mask').click(function () {
+		$('.mask').fadeOut(300);
+		$('.details-add').fadeOut(300);
+		$('body').removeClass('overflowY');
+	})
+
+})
+	
